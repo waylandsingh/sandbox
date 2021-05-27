4932feda62423d83348483a83169b8d2728bb0ef
waylandsingh <wmsingh@ucdavis.edu>
2021-05-27 14:28:27 -0700

    functionality all there, aesthetics not quite

--- Etch-a-Sketch/sketch.js
+++ Etch-a-Sketch/sketch.js
@@ -4,19 +4,23 @@
 const grid = document.querySelector('#grid')
 // iterate 16x16 times to create & append child
 const container = document.querySelector('#container')
-console.log(grid.style)
+const sideLength = container.clientHeight
 
 
 function drawSquare(n=16) {
     grid.innerHTML = ''
     grid.style.gridTemplateColumns = `repeat(${n},1fr)`;
     grid.style.gridTemplateRows = `repeat(${n},1fr)`;
+    const gridSqSize = calcGridSq(sideLength, n);
+
     // add a background image to 'uncover'
 
     for (let i=0; i<n*n; i++){
         // create, add the class, and append
         const sq = document.createElement('div')
         sq.classList.value = 'gridsq'
+        sq.style.width = gridSqSize;
+        sq.style.height = gridSqSize;
         grid.appendChild(sq)
 
         // attach event listeners
@@ -28,8 +32,19 @@ function drawSquare(n=16) {
     
 }
 
-// add function for the clear with limit 100
+// function to calculate 'gridsize'
+function calcGridSq(sideLengthPx, n) {
+    return `${sideLengthPx / n}px`
+}
 
+// size when the button is clicked
+function resize() {
+    // get user input...
+    const newDimension = prompt('input an integer for # of pixels')
+    
+    // and run the drawsquare function with the input
+    drawSquare(parseInt(newDimension))
+}
 
 // how to get the size of the container, and modify the css of resulting pixels?
-drawSquare(20)
+drawSquare(16)
